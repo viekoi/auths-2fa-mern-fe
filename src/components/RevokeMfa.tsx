ac8c28fbc570db15a58e091a7128b86b3e4e4758
khoi nguyen <vkhoi444@gmail.com>
2025-02-21 10:35:12 +0700

    auths-2fa-mern-fe

---
+++ src/components/RevokeMfa.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { toast } from "@/hooks/use-toast";
+import { revokeMFAMutationFn } from "@/lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import { useCallback } from "react";
+
+const RevokeMfa = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: revokeMFAMutationFn,
+    onSuccess: (response: any) => {
+      queryClient.invalidateQueries({
+        queryKey: ["authUser"],
+      });
+      toast({
+        title: "Success",
+        description: response.message,
+      });
+    },
+    onError: (error) => {
+      toast({
+        title: "Error",
+        description: error.message,
+        variant: "destructive",
+      });
+    },
+  });
+
+  const handleClick = useCallback(() => {
+    mutate();
+  }, []);
+
+  return (
+    <Button
+      disabled={isPending}
+      className="h-[35px] !text-[#c40006d3] !bg-red-100 shadow-none mr-1"
+      onClick={handleClick}
+    >
+      {isPending && <Loader className="animate-spin" />}
+      Revoke Access
+    </Button>
+  );
+};
+
+export default RevokeMfa;
