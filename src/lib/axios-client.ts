ae2cece273ddb028b81578c5648ef330fa08d818
Nguyễn Việt Khôi <97336479+viekoi@users.noreply.github.com>
2025-03-14 18:55:28 +0700

    Update axios-client.ts

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL | "localhost:8000";
 
 const options = {
   baseURL,
